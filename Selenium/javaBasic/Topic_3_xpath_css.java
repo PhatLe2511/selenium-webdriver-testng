3d958f918d3f677c59f6c622bbbc4f7b1cfdc200
Thanh Phat <phatl2511@gmail.com>
2021-12-02 20:27:04 +0700

    Topic_3_xpath_I

--- Selenium/javaBasic/Topic_3_xpath_css.java
+++ Selenium/javaBasic/Topic_3_xpath_css.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
